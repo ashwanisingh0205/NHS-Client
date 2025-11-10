77dce0a8899f216f17ea2dfd378ca6f6e53b743d
ashwani singh <118151369+ashwanisingh0205@users.noreply.github.com>
2025-11-10 06:14:26 +0530

    second

--- app/plugins/toast.client.js
+++ app/plugins/toast.client.js
@@ -3,19 +3,27 @@ import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  // Register toast plugin with professional settings
   nuxtApp.vueApp.use(Toast, {
     position: 'top-right',
-    timeout: 3000,
+    timeout: 4000,
     closeOnClick: true,
     pauseOnFocusLoss: true,
     pauseOnHover: true,
     draggable: true,
     draggablePercent: 0.6,
     showCloseButtonOnHover: false,
-    hideProgressBar: false,
+    hideProgressBar: true,
     closeButton: 'button',
     icon: true,
     rtl: false,
+    maxToasts: 5,
+    newestOnTop: true,
+    transition: 'Vue-Toastification__bounce',
+    transitionDuration: 750,
   })
+  
+  // Make toast available on nuxtApp.$toast
+  nuxtApp.$toast = nuxtApp.vueApp.config.globalProperties.$toast
 })
 
